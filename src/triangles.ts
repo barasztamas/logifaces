8737994172c36de1d2bffb107d22d9f9a10237ba
Tamas Barasz <barasztamas@yahoo.com>
2025-04-06 22:37:41 +0200

    set of stones in the puzzle

---
+++ src/triangles.ts
@@ -0,0 +1,24 @@
+type CornerHeight = 1 | 2 | 3;
+
+//**
+// * The triangles are defined clockwise from the top corner.
+//  */
+export type Triangle = [CornerHeight, CornerHeight, CornerHeight];
+export const triangles: Triangle[] = [
+    [1, 1, 1],
+    [1, 1, 2],
+    [1, 1, 2],
+    [2, 2, 1],
+    [1, 1, 3],
+    [1, 3, 3],
+    [2, 3, 3],
+    [3, 3, 3],
+    [3, 3, 2],
+    [1, 2, 3],
+    [1, 3, 2],
+    [1, 3, 2],
+    [2, 2, 3],
+    [2, 2, 3],
+    [1, 2, 3],
+    [2, 3, 3],
+];
