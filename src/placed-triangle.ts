03dcdce2bdd27436357b11c89789128877109241
Tamas Barasz <barasztamas@yahoo.com>
2025-04-10 09:30:41 +0200

    getCornerHeights

---
+++ src/placed-triangle.ts
@@ -0,0 +1,28 @@
+import { getCorners, Place } from './place';
+import { Triangle } from './triangles';
+
+export type PlacedTriangle = { place: Place; triangle: Triangle; rotation: 0 | 1 | 2 };
+
+export type CornerHeights = {
+    [x: number]: {
+        [y: number]: number;
+    };
+};
+export function getCornerHeights(placedTriangles: PlacedTriangle[]): CornerHeights {
+    const cornerHeights: CornerHeights = {};
+    for (const { place, triangle, rotation } of placedTriangles) {
+        const corners = getCorners(place);
+        for (let i = 0; i < 3; i++) {
+            const { x, y } = corners[i];
+            const height = triangle[(i + rotation) % 3];
+            if (!cornerHeights[x]) {
+                cornerHeights[x] = {};
+            }
+            if (cornerHeights[x][y] && cornerHeights[x][y] !== height) {
+                throw new Error(`Inconsistent height at (${x}, ${y}): ${cornerHeights[x][y]} vs ${height}`);
+            }
+            cornerHeights[x][y] = height;
+        }
+    }
+    return cornerHeights;
+}
