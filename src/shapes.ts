f8d1f6030b410b85f7ae6852ab5f838f3a4b668f
Tamas Barasz <barasztamas@yahoo.com>
2025-06-07 23:11:49 +0200

    rotateShape

--- src/shapes.ts
+++ src/shapes.ts
@@ -1,6 +1,43 @@
 import { Place } from './place';
 
-export const shapes: { [key: string]: Place[] } = {
+type Shape = Place[];
+const ShapeRotations = [0, 1, 2, 3, 4, 5] as const;
+type ShapeRotation = (typeof ShapeRotations)[number];
+
+export function rotationSymmetries(_shape: Shape) {
+    const shape = normalizeShape(_shape);
+    return ShapeRotations.filter((rotation) => JSON.stringify(shape) === JSON.stringify(rotateShape(shape, rotation)));
+}
+
+export function getShapeRotations(shape: Shape): Shape[] {
+    return ShapeRotations.map((rotation) => rotateShape(shape, rotation));
+}
+
+function rotateShape(shape: Shape, rotation: ShapeRotation): Shape {
+    if (rotation === 0) {
+        return normalizeShape(shape);
+    }
+    const rotatedShape: Shape = shape.map(({ x, y, direction }) => ({
+        x: x + y + (direction === 'up' ? 0 : 1),
+        y: -x,
+        direction: direction === 'up' ? 'down' : 'up',
+    }));
+    return rotateShape(rotatedShape, (rotation - 1) as ShapeRotation);
+}
+
+export function normalizeShape(shape: Shape): Shape {
+    const minX = Math.min(...shape.map(({ x }) => x));
+    const minY = Math.min(...shape.map(({ y }) => y));
+    return shape
+        .map(({ x, y, direction }) => ({
+            x: x - minX,
+            y: y - minY,
+            direction,
+        }))
+        .sort((a, b) => a.x - b.x || a.y - b.y || a.direction.localeCompare(b.direction));
+}
+
+export const shapes: { [key: string]: Shape } = {
     triangle: [
         { x: 1, y: 1, direction: 'up' },
         { x: 1, y: 1, direction: 'down' },
